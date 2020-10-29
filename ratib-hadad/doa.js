81c18f3dda69e11f72b39a1b95e3d7361a3c2b8f
Rifki Loen <flockring@gmail.com>
2020-10-29 21:16:40 +0700

    Add files via upload

---
+++ ratib-hadad/doa.js
@@ -0,0 +1,45 @@
+const doa = [
+	{
+		nama : "Sayidul Istighfar",
+		arab : "	للَّهُمَّ أَنْتَ رَبِّيْ لاَ إِلَـهَ إِلاَّ أَنْتَ، خَلَقْتَنِيْ وَأَنَا عَبْدُكَ، وَأَنَا عَلَى عَهْدِكَ وَوَعْدِكَ مَا اسْتَطَعْتُ، أَعُوْذُ بِكَ مِنْ شَرِّ مَا صَنَعْتُ، أَبُوْءُ لَكَ بِنِعْمَتِكَ عَلَيَّ، وَأَبُوْءُ بِذَنْبِيْ فَاغْفِرْ لِيْ فَإِنَّهُ لاَ يَغْفِرُ الذُّنُوْبَ إِلاَّ أَنْتَ",
+		latin : "Allahumma laka shumtu wa bika amantu wa'ala rizqika afthartu. Birrahmatika yaa arhamar roohimin.",
+		indo : "Ya Allah, untuk Mu aku berpuasa, dan kepada Mu aku beriman, dan dengan rezeki Mu aku berbuka. Dengan rahmat Mu wahai yang Maha Pengasih dan Penyayang.",
+		warna : 1
+	},
+	{
+		nama : "Sebelum Makan",
+		arab : "اَللّٰهُمَّ بَارِكْ لَنَا فِيْمَا رَزَقْتَنَا وَقِنَا عَذَابَ النَّارِ",
+		latin : "Alloohumma barik lanaa fiimaa razatanaa waqinaa 'adzaa bannar",
+		indo : "Ya Allah, berkahilah kami dalam rezeki yang telah Engkau berikan kepada kami dan peliharalah kami dari siksa api neraka",
+		warna : 4
+	},
+	{
+		nama : "Sesudah Makan",
+		arab : "اَلْحَمْدُ ِللهِ الَّذِىْ اَطْعَمَنَا وَسَقَانَا وَجَعَلَنَا مُسْلِمِيْنَ" ,
+		latin : "Alhamdu lillaahil ladzii ath'amanaa wa saqoonaa wa ja'alnaa muslimiin",
+		indo : "Segala puji bagi Allah yang telah memberi makan kami dan minuman kami, serta menjadikan kami sebagai orang-orang islam",
+		warna : 3
+	},
+	{
+		nama : "Ketika Makan Lupa Membaca Doa",
+		arab : "بِسْمِ اللهِ فِىِ أَوَّلِهِ وَآخِرِهِ",
+		latin : "Bismillaahi fii awwalihi wa aakhirihi",
+		indo : "Dengan menyebut nama Allah pada awal dan akhirnya",
+		warna: 4
+	},
+	{
+		nama : "Sebelum Tidur",
+		arab : "بِسْمِكَ اللّٰهُمَّ اَحْيَا وَاَمُوْتُ",
+		latin : "Bismikallohumma ahya wa amuutu",
+		indo : "Dengan menyebut nama-Mu ya Allah, aku hidup dan aku mati",
+		warna : 5
+	},
+	{
+		nama : "Masuk Kamar Mandi Atau Toilet",
+		arab : "اَللّٰهُمَّ اِنّىْ اَعُوْذُبِكَ مِنَ الْخُبُثِ وَالْخَبَآئِثِ",
+		latin : "Alloohumma Innii a'uudzubika minal khubutsi wal khoaaitsi",
+		indo : "Ya Allah, aku berlindung pada-Mu dari godaan syetan  laki-laki dan setan perempuan",
+		warna : 2
+	},
+
+];
